b753f6b1d94523a858207b56a05314936d9582cf
typeofadam <typeof.adam@gmail.com>
2025-10-04 18:47:33 +0100

    Added check for closing window and draw a filled black screen.

--- main.py
+++ main.py
@@ -5,9 +5,22 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    ##### Game Loop ####
+    while True:
+        # Test if user closed game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill("black")
+        # ALWAYS END LOOP WITH FLIP
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
