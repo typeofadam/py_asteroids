58d1a6bf48ee5d62c22a2d67c1d393e25bb3c85e
typeofadam <typeof.adam@gmail.com>
2025-10-04 18:53:49 +0100

    capped to 60fps and added dt var

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ import pygame
 from constants import *
 
 def main():
+    ### Create pygame game stuff
     pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -19,8 +23,9 @@ def main():
                 return
         
         screen.fill("black")
-        # ALWAYS END LOOP WITH FLIP
+        # ALWAYS END LOOP WITH FLIP AND TICK
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
